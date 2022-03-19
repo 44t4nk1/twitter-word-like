770a136495c458c9feca1f0ae427374a5dc1110c
SIddharth SIngh <siddharth.singh2019@vitstudent.ac.in>
2022-03-19 11:20:59 +0530

    added user tweets and likes fetch

--- api/controller/tweets.go
+++ api/controller/tweets.go
@@ -22,11 +22,47 @@ func GetTweets(c *fiber.Ctx) error {
 		log.Fatal("Error loading .env file")
 	}
 
-	url := os.Getenv("TWITTER_USERNAME_URL") + username
+	data := GetTwitterID(os.Getenv("TWITTER_URL") + "by/username/" + username)
 
-	data := GetTwitterID(url)
+	tweets := GetTweetsByID(os.Getenv("TWITTER_URL") + data.Data.ID + "/tweets?exclude=retweets,replies&tweet.fields=public_metrics&max_results=100")
 
-	return c.Status(200).JSON(data.Data)
+	return c.Status(200).JSON(tweets.Data)
+}
+
+func GetTweetsByID(url string) models.UserTweetList {
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	err = godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	req.Header.Set("Authorization", os.Getenv("BEARER"))
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	res, err := client.Do(req)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	defer res.Body.Close()
+
+	var data models.UserTweetList
+
+	json.NewDecoder(res.Body).Decode(&data)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	return data
 }
 
 func GetTwitterID(url string) models.UserTwitterBase {
--- api/models/models.go
+++ api/models/models.go
@@ -9,3 +9,28 @@ type UserTwitterDetails struct {
 type UserTwitterBase struct {
 	Data UserTwitterDetails `json:"data"`
 }
+
+type UserTweet struct {
+	PublicMetrics UserTweetMetrics `json:"public_metrics"`
+	ID            string           `json:"id"`
+	Text          string           `json:"text"`
+}
+
+type UserTweetMetrics struct {
+	RetweetCount int `json:"retweet_count"`
+	ReplyCount   int `json:"reply_count"`
+	LikeCount    int `json:"like_count"`
+	QuoteCount   int `json:"quote_count"`
+}
+
+type UserTweetMeta struct {
+	OldestID    string `json:"oldest_id"`
+	NewestID    string `json:"newest_id"`
+	ResultCount int    `json:"result_count"`
+	NextToken   string `json:"next_token"`
+}
+
+type UserTweetList struct {
+	Data []UserTweet   `json:"data"`
+	Meta UserTweetMeta `json:"meta"`
+}
